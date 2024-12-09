8d8f1642a5768b79c1ca6db92f09ebe602bc0109
MilleriouxMaxime <milleriouxmaxime@gmail.com>
2024-12-09 23:22:05 +0100

    Add Unsubscribe player

--- controllers/tournament_manager.py
+++ controllers/tournament_manager.py
@@ -39,7 +39,7 @@ class TournamentManager:
 
         tournaments_table.insert(tournament.to_dict())
 
-        print_success("Tournament created successfully!")
+        print_success(f"Tournament '{tournament.name}' created successfully!")
 
     def subscribe_players_to_tournament(self):
         """
@@ -50,7 +50,7 @@ class TournamentManager:
         player = players_table.get(Query().identifier == player_id)
 
         if player is None:
-            print_error("Player not found!")
+            print_error(f"Player '{player_id}' not found!")
             return
 
         tournament_name = input("Enter tournament name: ")
@@ -58,13 +58,48 @@ class TournamentManager:
         tournament = tournaments_table.get(Query().name == tournament_name)
 
         if tournament is None:
-            print_error("Tournament not found!")
+            print_error(f"Tournament '{tournament_name}' not found!")
             return
 
         tournament["players_list"].append(player_id)
         tournaments_table.update(tournament, Query().name == tournament_name)
 
-        print_success("Player subscribed to tournament successfully!")
+        print_success(
+            f"Player '{player_id}' subscribed to tournament '{tournament_name}' successfully!"
+        )
+
+    def unsubscribe_players_from_tournament(self):
+        """
+        Désinscription d'un joueur à un tournoi en utilisant son ID pour le retrouver dans la base de données
+        """
+
+        player_id = input("Enter player ID: ")
+        # Check if player exist
+        player = players_table.get(Query().identifier == player_id)
+
+        if player is None:
+            print_error(f"Player '{player_id}' not found!")
+            return
+
+        tournament_name = input("Enter tournament name: ")
+        # Check if tounament exist
+        tournament = tournaments_table.get(Query().name == tournament_name)
+
+        if tournament is None:
+            print_error(f"Tournament '{tournament_name}' not found!")
+            return
+        if player_id not in tournament["players_list"]:
+            print_error(
+                f"Player '{player_id}' not subscribed to tournament '{tournament_name}'!"
+            )
+            return
+
+        tournament["players_list"].remove(player_id)
+        tournaments_table.update(tournament, Query().name == tournament_name)
+
+        print_success(
+            f"Player '{player_id}' unsubscribed from tournament '{tournament_name}' successfully!"
+        )
 
     def run(self):
         while True:
@@ -72,11 +107,14 @@ class TournamentManager:
             print("\n Tournoi \n")
             print("1. Creer un tournoi")
             print("2. Inscrire un joueur")
+            print("3. Désinscrire un joueur")
             print("q. Quitter")
             choice = input("Votre choix: ")
             if choice == "1":
                 self.create_tournament()
             elif choice == "2":
                 self.subscribe_players_to_tournament()
+            elif choice == "3":
+                self.unsubscribe_players_from_tournament()
             elif choice == "q":
                 break
